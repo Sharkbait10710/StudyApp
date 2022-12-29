e8c9de46554eebf204a8007dcdd71cf63cca1b7f
Dang Le <Dang.Le10710@gmail.com>
2022-12-29 14:07:27 -0800

    [backend] currently able to read from readable stream

--- app/server/index.js
+++ app/server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
-
+const serverUrl = 'http://localhost:' + PORT;
 const app = express();
+
+let num = 0; 
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send("something");
 });
