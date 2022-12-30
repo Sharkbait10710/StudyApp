6c489c960d41040d757a210b188bf0354598dc1e
Dang Le <Dang.Le10710@gmail.com>
2022-12-29 19:30:04 -0800

    [backend] can read and write from database

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const body_parser = require('body-parser');
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,10 +14,23 @@ app.use(cors());
 app.use(body_parser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.end("Initialize");
+  fs.readFile("data.json", function(err, data) {
+      
+    // Check for errors
+    if (err) throw err;
+   
+    // Converting to JSON
+    const users = JSON.parse(data);
+      
+    res.end(users);
+  });
 });
 
 app.post('/', function requestHandler(req, res) {
   res.end(JSON.stringify(req.body));
   console.log(JSON.stringify(req.body));
+  fs.writeFile("data.json", JSON.stringify(JSON.stringify(req.body)), err => {
+    if (err) throw err; 
+    console.log("Done writing");
+  });
 });
